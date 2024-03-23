3a1ce9760763206a0a4dc6e0b7beb7a17f1ca338
marcoserenelli <marco.serenelli00@gmail.com>
2024-03-23 13:19:40 +0100

    Changed logic to match new Minecraft 1.20. Removed splash water effect as is now in official minecraft, updated just the drink potion effect

--- src/main/java/com/mrawsaf/splash/EventHandler.java
+++ src/main/java/com/mrawsaf/splash/EventHandler.java
@@ -1,35 +1,16 @@
 package com.mrawsaf.splash;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.projectile.PotionEntity;
-import net.minecraft.entity.projectile.ProjectileItemEntity;
-import net.minecraft.entity.projectile.ThrowableEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.PotionItem;
-import net.minecraft.potion.*;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraftforge.event.entity.ProjectileImpactEvent;
+import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraft.world.item.alchemy.Potions;
 
-import java.util.function.Predicate;
 
 @Mod.EventBusSubscriber(modid = Splash.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class EventHandler {
-
-    @SubscribeEvent
-    public static void onWaterPotionThrown(final ProjectileImpactEvent.Throwable event) {
-        ThrowableEntity throwable = event.getThrowable();
-        if(throwable instanceof PotionEntity){
-            if(PotionUtils.getPotion(((ProjectileItemEntity)throwable).getItem()) == Potions.WATER){
-                AxisAlignedBB area = throwable.getBoundingBox().inflate(4, 2,4);
-                Predicate<LivingEntity> predicate = entity-> entity.isOnFire() && throwable.distanceToSqr(entity) < 4 * 4 && !entity.isSensitiveToWater();
-                throwable.level.getEntitiesOfClass(LivingEntity.class, area, predicate).forEach(Entity::clearFire);
-            }
-        }
-    }
 
     @SubscribeEvent
     public static void onWaterPotionDrink(LivingEntityUseItemEvent.Finish event){
